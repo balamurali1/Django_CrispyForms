2a20fe7804b090e028e01e3950ee0a1ed20a344e
Balamurali <balamurali1@gmail.com>
2021-09-06 19:09:28 +0530

    GET & POST

---
+++ gs55/enroll/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from enroll import forms
+
+# Create your views here.
+
+def showinfo(request):
+	fm = forms.StudentRegistration()
+
+	return render(request,'enroll/user.html',{'form':fm})
