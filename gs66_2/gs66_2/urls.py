3cabc9c0091192467e08ed80eee66b20c69918f5
Balamurali <balamurali1@gmail.com>
2021-09-15 17:23:30 +0530

    Dynamic URL <str:my_id>

---
+++ gs66_2/gs66_2/urls.py
@@ -0,0 +1,29 @@
+"""gs66_2 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from enroll import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.home,name='home'),
+    #path('student/<str:my_id>/',views.show,name='detail')
+    	#(OR)
+    path('student/<my_id>/',views.show,name='detail')
+]
+
+#by default ga <my_id> anedi string ga untundi...
+
