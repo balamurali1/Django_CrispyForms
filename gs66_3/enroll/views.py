ebbf2b0216fab54606a3271e491a25420bb43ad9
Balamurali <balamurali1@gmail.com>
2021-09-15 17:24:46 +0530

    Dynamic URL <int:<my_id>

---
+++ gs66_3/enroll/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+	return render(request,'enroll/home.html')
+
+def show(request,my_id):
+	if my_id == 1:
+		student = {'id':my_id,'name':'Rohan'}
+
+	if my_id == 2:
+		student = {'id':my_id,'name':'sonam'}
+
+	if my_id == 3:
+		student = {'id':my_id,'name':'murali'}
+
+	return render(request,'enroll/show.html', student)	
