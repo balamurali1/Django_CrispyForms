2eaf54928a4373c5bceb0116b6f455b5adf5a442
Balamurali <balamurali1@gmail.com>
2021-09-23 18:26:51 +0530

    filter, &(and), |(or), ~(Negation operator)

---
+++ gs100_Q_Objects/enroll/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from enroll import models
+
+# Register your models here.
+
+@admin.register(models.Student)
+class StudentAdmin(admin.ModelAdmin):
+	list_display = ['id','name','roll','city','marks','passdate','admdatetime']
---
+++ gs100_Q_Objects/enroll/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from enroll.models import Student
+from django.db.models import Q
+
+# Create your views here.
+
+def home(request):
+
+	########## all() ##########
+
+	#student_date = Student.objects.all()
+
+	######## & #########
+	#student_date = Student.objects.filter(Q(id=6) & Q(roll=106))
+
+	#student_date = Student.objects.filter(Q(name='Ramesh') & Q(marks=56))
+
+	#student_date = Student.objects.filter(Q(id=2) & Q(marks=50)) # record not visible because both are not True
+
+
+	################ | ##############
+
+	#student_date = Student.objects.filter(Q(id =3) | Q(roll=108))
+
+	#student_date = Student.objects.filter(Q(marks = 89) | Q(city='kurnool'))
+	
+	#student_date = Student.objects.filter(Q(marks =45) | Q(city='Hyderand'))
+
+	#student_date = Student.objects.filter(Q(marks =45) | Q(name = 'Murali'))
+
+	
+	############## ~ (Negation Operator) ################
+
+	student_date = Student.objects.filter(~Q(id =3))
+
+	#student_date = Student.objects.filter(~Q(id =1))
+
+
+	print("Return:",student_date)
+	print()
+	print('SQL Query:',student_date.query)
+
+	return render (request,'enroll/home.html',context={'student':student_date})
