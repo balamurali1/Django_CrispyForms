1ac291ec8cd22b65a97bc572b1c1c9f6e6c5e176
Balamurali <balamurali1@gmail.com>
2021-09-27 19:45:07 +0530

    ManyToMany

---
+++ gs106_ManyToManyRelationship/enroll/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from enroll.models import Song
+# Register your models here.
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+	list_display = ['song_name','song_duration','written_by']
